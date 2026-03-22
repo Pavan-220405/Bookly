34fe140f2effc8bf2f2c48367a1f0bb6e51a2d58
Pavan G <pavan220405@gmail.com>
2026-03-22 23:04:31 +0530

    Moved from __init__.py to main.py

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,50 +1,50 @@
-from fastapi import FastAPI
-from contextlib import asynccontextmanager
+# from fastapi import FastAPI
+# from contextlib import asynccontextmanager
 
-from myapp.db.engine import init_db, close_db, get_pool
-from myapp.db.redis_engine import init_redis, get_redis, close_redis
-from myapp.books.models import create_books_table
-from myapp.users.models import create_users_table
+# from myapp.db.engine import init_db, close_db, get_pool
+# from myapp.db.redis_engine import init_redis, get_redis, close_redis
+# from myapp.books.models import create_books_table
+# from myapp.users.models import create_users_table
 
-from myapp.books.routes import book_router
-from myapp.users.routes import auth_router
-
-
-
-@asynccontextmanager
-async def life_span(app : FastAPI):
-    print("Server is starting...")
-
-    # Initialize pool
-    await init_db()
-    await init_redis()
-
-    # Create tables
-    pool = get_pool()
-    # await create_books_table(pool=pool) --> Done by Alembic
-    # await create_users_table(pool=pool) --> Done by Alembic
-    print("Databases and Tables initialized !!!")
-
-    yield
-
-    print("Server is shutting down...")
-    await close_db()
-    await close_redis()
+# from myapp.books.routes import book_router
+# from myapp.users.routes import auth_router
 
 
 
-version = "v1"
-app = FastAPI(
-    title="Bookly - Book Review API",
-    description="A REST API for book review web service",
-    version=version,
-    lifespan=life_span
-    )
+# @asynccontextmanager
+# async def life_span(app : FastAPI):
+#     print("Server is starting...")
+
+#     # Initialize pool
+#     await init_db()
+#     await init_redis()
+
+#     # Create tables
+#     pool = get_pool()
+#     # await create_books_table(pool=pool) --> Done by Alembic
+#     # await create_users_table(pool=pool) --> Done by Alembic
+#     print("Databases and Tables initialized !!!")
+
+#     yield
+
+#     print("Server is shutting down...")
+#     await close_db()
+#     await close_redis()
 
 
-@app.get('/',tags=["Health Check"])
-async def health_check():
-    return {"status" : "Bookly API Running Successfully"} 
 
-app.include_router(book_router,prefix=f"/api/{version}/books",tags=['Books'])
-app.include_router(auth_router,prefix=f"/api/{version}/users",tags=['Users'])
+# version = "v1"
+# app = FastAPI(
+#     title="Bookly - Book Review API",
+#     description="A REST API for book review web service",
+#     version=version,
+#     lifespan=life_span
+#     )
+
+
+# @app.get('/',tags=["Health Check"])
+# async def health_check():
+#     return {"status" : "Bookly API Running Successfully"} 
+
+# app.include_router(book_router,prefix=f"/api/{version}/books",tags=['Books'])
+# app.include_router(auth_router,prefix=f"/api/{version}/users",tags=['Users'])
---
+++ myapp/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from myapp.db.engine import init_db, close_db, get_pool
+from myapp.db.redis_engine import init_redis, close_redis
+
+from myapp.books.routes import book_router
+from myapp.users.routes import auth_router
+
+
+
+@asynccontextmanager
+async def life_span(app : FastAPI):
+    print("Server is starting...")
+
+    # Initialize pool
+    await init_db()
+    await init_redis()
+
+    # Create tables
+    pool = get_pool()
+    # await create_books_table(pool=pool) --> Done by Alembic
+    # await create_users_table(pool=pool) --> Done by Alembic
+    print("Databases and Tables initialized !!!")
+
+    yield
+
+    print("Server is shutting down...")
+    await close_db()
+    await close_redis()
+
+
+
+version = "v1"
+app = FastAPI(
+    title="Bookly - Book Review API",
+    description="A REST API for book review web service",
+    version=version,
+    lifespan=life_span
+    )
+
+
+@app.get('/',tags=["Health Check"])
+async def health_check():
+    return {"status" : "Bookly API Running Successfully"} 
+
+app.include_router(book_router,prefix=f"/api/{version}/books",tags=['Books'])
+app.include_router(auth_router,prefix=f"/api/{version}/users",tags=['Users'])
